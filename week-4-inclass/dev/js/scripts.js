3887a5381c297656e8e70229b7649e3b8f5d4bcf
allymazza <102631621+allymazza@users.noreply.github.com>
2022-10-18 19:21:54 -0400

    gsap

--- week-4-inclass/dev/js/scripts.js
+++ week-4-inclass/dev/js/scripts.js
@@ -1,16 +1,30 @@
-import $ from "jquery";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
-let $nav = $('nav[data-nav]');
-let isVisible = false;
+gsap.registerPlugin(ScrollTrigger);
 
-$(".nav-btns").on("click", function(){
-    console.log("click");
-    if(isVisible === false){
-        $nav.show();
-        isVisible = true;
-    }
-    else{
-        $nav.hide();
-        isVisible = false;
-    }
-})
+function heroAnimation(){
+    var tl = gsap.timeline();
+    tl.from("#hero", {duration: 2, opacity: 0}, {opacity: 1, y: 0});
+
+    return tl;
+}
+        
+function historyAnimation(){
+    var tl = gsap.timeline();
+    tl.from("#history-first", {duration:1,x:200,alpha:0, scrollTrigger:{ trigger: "#history-first"}})
+        .from("#history-middle", {duration:1,y:200,alpha:0, scrollTrigger:{ trigger: "#history-middle"}})
+        .from("history-last", {duration:0.5,rotation:90, alpha:0, transformOrigin: "left bottom", scrollTrigger:{ trigger: "#history-last"}});
+
+    return tl;
+}
+
+function skillsAnimation(){
+    var tl = gsap.timeline();
+    tl.from("#emotion", {duration:1, x:200, alpha:0, scrollTrigger: { trigger: "#emotion"}})
+
+var mainTimeline = gsap.timeline();
+mainTimeline.add(heroAnimation())
+    .add(historyAnimation())
+    .add(skillsAnimation())
+    // .add(galleryAnimation())
