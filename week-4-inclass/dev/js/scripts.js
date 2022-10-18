9ea445409b87aad3fa2a701cd07e4f19eee64abc
allymazza <102631621+allymazza@users.noreply.github.com>
2022-10-18 13:35:38 -0400

    new

--- week-4-inclass/dev/js/scripts.js
+++ week-4-inclass/dev/js/scripts.js
@@ -1,35 +1,16 @@
-// console.log("hello class");
-import { gsap } from "gsap";
+import $ from "jquery";
 
-// Sets
-gsap.set("#hero h1 span",{alpha:0.25})
+let $nav = $('nav[data-nav]');
+let isVisible = false;
 
-var heroButtonTL = gsap.timeline({paused:true});
-    heroButtonTL.to("#trails-btn",{duration:0.25,scale:2, backgroundColor:"#881d02"},"trigger")  
-    .to("#first-line",{duration:0.25, alpha:0, y:50},"trigger")
-    .to("#second-line",{duration:0.25, alpha:0, y:20},"trigger")
-    .to("#trails-btn i",{duration:0.25, rotateY:180},"-=0.15");
-
-
-var trailsBtn = document.querySelector("#trails-btn");
-
-trailsBtn.addEventListener("mouseover",function(){
-    heroButtonTL.play();
+$(".nav-btns").on("click", function(){
+    console.log("click");
+    if(isVisible === false){
+        $nav.show();
+        isVisible = true;
+    }
+    else{
+        $nav.hide();
+        isVisible = false;
+    }
 })
-
-trailsBtn.addEventListener("mouseout",function(){
-    heroButtonTL.reverse();
-})
-
-function heroAnimation(){
-    var tl = gsap.timeline();
-    tl.from("#first-line",{duration:1,alpha:0, y:-100})
-    .from("#second-line",{duration:1,alpha:0, y:-100},"-=0.75")
-    .from("#trails-btn",{duration:1,y:100, alpha:0},"-=.5")
-    .from("#trails-btn i",{duration:0.5,rotation:90, alpha:0, transformOrigin: "left bottom"},"-=0.5");
-    return tl;
-}
-
-
-var mainTimeline = gsap.timeline();
-mainTimeline.add(heroAnimation());
