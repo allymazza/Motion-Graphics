0e665fbcc772c2a61669687a84cb8938cf52f9af
allymazza <102631621+allymazza@users.noreply.github.com>
2022-10-20 11:54:42 -0400

    scroll trigger

--- week-4-inclass/dev/js/scripts.js
+++ week-4-inclass/dev/js/scripts.js
@@ -23,8 +23,7 @@ function historyAnimation(){
                 trigger: "#history-first", 
                 scrub: true, 
                 start:"top 70%", 
-                end:"bottom 10%",
-                markers: false}})
+                end:"bottom 10%"}})
 
          .from("#history-middle", 
          {duration:1,
@@ -34,8 +33,7 @@ function historyAnimation(){
                 trigger: "#history-middle", 
                 scrub: true, 
                 start:"top 90%", 
-                end:"bottom 10%",
-                markers: false}})
+                end:"bottom 10%"}})
 
         .from("history-last", 
         {duration:0.5,
@@ -46,8 +44,7 @@ function historyAnimation(){
                  trigger: "#history-last", 
                 scrub: true, 
                 start:"top 70%", 
-                end:"bottom 10%", 
-                markers: false}});
+                end:"bottom 10%"}});
 
     return tl;
 }
@@ -62,8 +59,7 @@ function skillsAnimation(){
             trigger: "#emotion", 
             scrub: true, 
             tart:"top 70%", 
-            end:"bottom 10%",
-            markers: false}})
+            end:"bottom 10%"}})
         .from("#learning", {duration:1, 
             x:200, 
             alpha:0, 
@@ -71,8 +67,7 @@ function skillsAnimation(){
                 trigger: "#learning", 
                 scrub: true, 
                 start:"top 70%", 
-                end:"bottom 10%", 
-                markers: false}})
+                end:"bottom 10%"}})
         .from("#math", 
             {duration:1, 
             x:-200, 
@@ -81,36 +76,99 @@ function skillsAnimation(){
                 trigger: "#math", 
                 scrub: true, 
                 start:"top 70%", 
-                end:"bottom 10%",
-                markers: false}})
-
+                end:"bottom 10%"}})
+        .from("#aside",
+        {duration: 1,
+        scale: 3,
+        alpha:0,
+        scrollTrigger:{ 
+            trigger: "#aside", 
+            scrub: true, 
+            tart:"top 70%", 
+            end:"bottom 10%"}})
 
      return tl;
 
-
 }
-
 
 function hero2Animation(){
     var tl = gsap.timeline();
     tl.from("#hero-2",
         {duration:1,
             clipPath:"inset(0 60%)",
-        // scale:2,
-        // alpha:0, 
         scrollTrigger:{ 
             trigger: "#math", 
             scrub: true, 
             start:"top 70%", 
             end:"bottom 10%",
             markers: false}})
+
+    return tl;
+ }
+
+ function skynetAnimation(){
+    var tl = gsap.timeline();
+    tl.from("gallery-1", {
+        duration:1,
+        x: -200,
+        alpha:0,
+        scrollTrigger:{
+            trigger: "gallery-1",
+            scrub: true, 
+            start:"top 50%", 
+            end:"bottom 10%",
+            markers: false}})
+         .from("gallery-2", {
+            duration:1,
+            x: 200,
+            alpha:0,
+                scrollTrigger:{
+                    trigger: "gallery-2",
+                    scrub: true, 
+                    start:"top 50%", 
+                    end:"bottom 10%",
+                    markers: false}})
+        .from("gallery-3", {
+            duration:1,
+            y: -200,
+            alpha:0,
+                scrollTrigger:{
+                    trigger: "gallery-3",
+                    scrub: true, 
+                    start:"top 50%", 
+                    end:"bottom 10%",
+                    markers: true}})
+        .from("gallery-4", {
+            duration:1,
+            x: -200,
+            alpha:0,
+                scrollTrigger:{
+                    trigger: "gallery-4",
+                    scrub: true, 
+                    start:"top 50%", 
+                    end:"bottom 10%",
+                    markers: true}})
+        .from("gallery-5", {
+            duration:1,
+            y: 200,
+            alpha:0,
+                scrollTrigger:{
+                    trigger: "gallery-5",
+                    scrub: true, 
+                    start:"top 50%", 
+                    end:"bottom 10%",
+                    markers: true}})
         
-    }
+        
+                
+        
 
-
+    return tl;
+ }
 
 var mainTimeline = gsap.timeline();
 mainTimeline.add(heroAnimation())
     .add(historyAnimation())
     .add(skillsAnimation())
     .add(hero2Animation())
+    .add(skynetAnimation())
