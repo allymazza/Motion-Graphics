5d9a8a2e13bb8bb4eb356c359169e9043916983a
allymazza <102631621+allymazza@users.noreply.github.com>
2022-10-24 13:51:28 -0400

    scroll trigger

--- week-4-inclass/dev/js/scripts.js
+++ week-4-inclass/dev/js/scripts.js
@@ -12,156 +12,134 @@ function heroAnimation(){
     return tl;
 }
         
+
+
 function historyAnimation(){
-    var tl = gsap.timeline();
+    var tl = gsap.timeline({scrollTrigger:{ 
+        trigger: "#history", 
+        scrub: true, 
+        start:"top 60%", 
+        end:"bottom bottom"}});
     
 
-    tl.from("#history-first", {duration:1,
+    tl.from("#history-first h1", 
+            {duration:2,
             x:200,
-            alpha:0, 
-            scrollTrigger:{ 
-                trigger: "#history-first", 
-                scrub: true, 
-                start:"top 70%", 
-                end:"bottom 10%"}})
+            alpha:0})
+        .from("#history-first h4", 
+            {duration:1,
+            x:-200,
+            alpha:0})
+        .from("history-first img",
+            {duration:1,
+            y:-200,
+            rotation:360,
+            alpha:0})
 
          .from("#history-middle", 
          {duration:1,
             y:200,
-            alpha:0, 
-            scrollTrigger:{ 
-                trigger: "#history-middle", 
-                scrub: true, 
-                start:"top 90%", 
-                end:"bottom 10%"}})
+            alpha:0})
 
         .from("history-last", 
-        {duration:0.5,
+        {duration:1,
             y:-200, 
-            alpha:0, 
-            transformOrigin: "left bottom", 
-            scrollTrigger:{
-                 trigger: "#history-last", 
-                scrub: true, 
-                start:"top 70%", 
-                end:"bottom 10%"}});
+            alpha:0});
 
     return tl;
 }
 
 function skillsAnimation(){
-    var tl = gsap.timeline();
-    tl.from("#emotion", 
+    var tl = gsap.timeline({scrollTrigger:{ 
+        trigger: "#skills", 
+        scrub: true, 
+        tart:"top 70%", 
+        end:"bottom 30%"}});
+
+    tl.from("#skills-content h1", 
         {duration:1, 
-        x:-200, 
-        alpha:0, 
-        scrollTrigger:{ 
-            trigger: "#emotion", 
-            scrub: true, 
-            tart:"top 70%", 
-            end:"bottom 10%"}})
-        .from("#learning", {duration:1, 
+        y:-200, 
+         alpha:0})
+         .from("#skills-content h4", 
+            {scale:3, 
+            rotation:360,
+            alpha:0})
+        .from("#skills-content p", 
+            {duration:1, 
+            y:200,
+            alpha:0})
+        .from("#emotion", 
+            {duration:1, 
+            x:-200, 
+            alpha:0})
+        .from("#learning", 
+            {duration:1, 
             x:200, 
-            alpha:0, 
-            scrollTrigger:{ 
-                trigger: "#learning", 
-                scrub: true, 
-                start:"top 70%", 
-                end:"bottom 10%"}})
+            alpha:0})
         .from("#math", 
             {duration:1, 
             x:-200, 
-            alpha:0, 
-            scrollTrigger:{ 
-                trigger: "#math", 
-                scrub: true, 
-                start:"top 70%", 
-                end:"bottom 10%"}})
-        .from("#aside",
+            alpha:0})
+        .from("#skills aside div",
         {duration: 1,
         scale: 3,
-        alpha:0,
-        scrollTrigger:{ 
-            trigger: "#aside", 
-            scrub: true, 
-            tart:"top 70%", 
-            end:"bottom 10%"}})
+        alpha:0})
 
      return tl;
 
 }
 
 function hero2Animation(){
-    var tl = gsap.timeline();
+    var tl = gsap.timeline({scrollTrigger:{ 
+        trigger: "#hero-2", 
+        scrub: true, 
+        start:"top 70%", 
+        end:"bottom 10%",
+        markers: false}});
     tl.from("#hero-2",
         {duration:1,
-            clipPath:"inset(0 60%)",
-        scrollTrigger:{ 
-            trigger: "#math", 
-            scrub: true, 
-            start:"top 70%", 
-            end:"bottom 10%",
-            markers: false}})
+            clipPath:"inset(0 60%)"})
 
     return tl;
  }
 
  function skynetAnimation(){
-    var tl = gsap.timeline();
-    tl.from("gallery-1", {
+    var tl = gsap.timeline({scrollTrigger:{
+        trigger: "#skynet",
+        scrub: true, 
+        start:"top 40%", 
+        end:"bottom 10%",
+        markers: false}});
+
+    tl.from("gallery-7", {
         duration:1,
-        x: -200,
-        alpha:0,
-        scrollTrigger:{
-            trigger: "gallery-1",
-            scrub: true, 
-            start:"top 50%", 
-            end:"bottom 10%",
-            markers: false}})
+        scale:3,
+        rotation:360,
+        alpha:0})
+        .from("gallery-1", {
+            duration:1,
+            x: -200,
+            alpha:0})
          .from("gallery-2", {
             duration:1,
             x: 200,
-            alpha:0,
-                scrollTrigger:{
-                    trigger: "gallery-2",
-                    scrub: true, 
-                    start:"top 50%", 
-                    end:"bottom 10%",
-                    markers: false}})
+            alpha:0})
         .from("gallery-3", {
             duration:1,
             y: -200,
-            alpha:0,
-                scrollTrigger:{
-                    trigger: "gallery-3",
-                    scrub: true, 
-                    start:"top 50%", 
-                    end:"bottom 10%",
-                    markers: true}})
+            alpha:0})
         .from("gallery-4", {
             duration:1,
             x: -200,
-            alpha:0,
-                scrollTrigger:{
-                    trigger: "gallery-4",
-                    scrub: true, 
-                    start:"top 50%", 
-                    end:"bottom 10%",
-                    markers: true}})
+            alpha:0})
         .from("gallery-5", {
             duration:1,
             y: 200,
-            alpha:0,
-                scrollTrigger:{
-                    trigger: "gallery-5",
-                    scrub: true, 
-                    start:"top 50%", 
-                    end:"bottom 10%",
-                    markers: true}})
-        
-        
-                
-        
+            alpha:0})
+        .from("gallery-6", {
+            duration:1,
+            x: 200,
+            alpha:0})
 
     return tl;
  }
