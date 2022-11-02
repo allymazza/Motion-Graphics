a4aaab5398b663f3deab52d57ffc2e52cfde5161
allymazza <102631621+allymazza@users.noreply.github.com>
2022-11-02 15:14:04 -0400

    UI

--- week-6-DrawSVG/dev/js/scripts.js
+++ week-6-DrawSVG/dev/js/scripts.js
@@ -11,29 +11,29 @@ function simpleMotion(){
     // gsap.set("#right-arrow",{x:-line.width/2 - leftArrow.width / 2, transformOrigin:"center" })
 
     var tl = gsap.timeline()
-    tl.from("#right-arrow",{scale:0, duration:0.25, drawSVG: 0})
+    tl.from("#right-arrow",{scale:0, duration:0.25, drawSVG: 0, stagger:.25})
     .from("#left-arrow",{scale:0, duration:0.25, drawSVG: 0})
-    .from("#right-arrow",{x:100, duration:0.25, drawSVG: 0})
-    .from("#left-arrow",{x:-100, duration:0.25, drawSVG: 0})
     .from("#up-arrow",{ scale:0, duration:0.25, stagger:0.25, drawSVG: 0})
     .from("#down-arrow",{ scale:0, duration:0.25, stagger:0.25, drawSVG: 0})
-    .from("#star",{scale:4, duration:0.25, drawSVG: 0})
+    .from("#star",{scale:4, opacity:3, duration:0.25, drawSVG: 0})
     
 }
-
-
 
 function patternMotion(){
     var tl = gsap.timeline()
     tl.from(".one",{duration:1, drawSVG:0, stagger:.25, transformOrigin:"center"},"pattern")
-    .fromTo(".two",{drawSVG:"0% 0%"},{duration:1, drawSVG:"0% -100%", stagger:0.25,},"pattern")
+    .fromTo(".two",{drawSVG:"0% 0%"},{duration:1, drawSVG:"0% -100%", stagger:0.25},"pattern")
     return tl;
 }
 
 
 function UIMotion(){
-
+    var tl = gsap.timeline()
+    tl.from(".letter",{duration:0.5, alpha:0, x:20, stagger: 0.25},"-=50%")
+    .from("#box",{drawSVG:"100% 100%"},{duration:1, drawSVG:"50% 50%"},"drawOutline")
+    return tl;
 }
+
 
 
 var mainTL = gsap.timeline();
