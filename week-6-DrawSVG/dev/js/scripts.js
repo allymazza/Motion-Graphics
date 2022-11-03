bd9ccb4019975078073cb638b2026ab52d60ee91
allymazza <102631621+allymazza@users.noreply.github.com>
2022-11-02 20:45:47 -0400

    UI

--- week-6-DrawSVG/dev/js/scripts.js
+++ week-6-DrawSVG/dev/js/scripts.js
@@ -6,15 +6,27 @@ gsap.registerPlugin(DrawSVGPlugin, GSDevTools);
 
 function simpleMotion(){
 
-     
-    // gsap.set("#horizontal",{x:line.width / 2 + leftArrow.width / 2, transformOrigin:"center" })
-    // gsap.set("#vertical",{x:-line.width/2 - leftArrow.width / 2, transformOrigin:"center" })
+    // var line = document.querySelector("#center");
+    
+    // line = line.getBBox();
+   
+
+    // var leftArrow = document.querySelector("#left-arrow");
+    // leftArrow = leftArrow.getBBox();
+
+    // var center = document.querySelector("#center");
+    // center = center.getBBox();
+   
+
+    // gsap.set("#left-arrow",{x:line.width / 4 + leftArrow.width / 2, transformOrigin:"center" })
+    // gsap.set("#right-arrow",{x:-line.width/ 4 - leftArrow.width / 2, transformOrigin:"center" })
+
 
     var tl = gsap.timeline()
-    tl.from("#right-arrow",{scale:0, duration:0.25, drawSVG: 0, stagger:.25})
-    .from("#left-arrow",{scale:0, duration:0.25, drawSVG: 0})
-    .from("#up-arrow",{ scale:0, duration:0.25, stagger:0.25, drawSVG: 0})
-    .from("#down-arrow",{ scale:0, duration:0.25, stagger:0.25, drawSVG: 0})
+    tl.from("#right-arrow",{scale:0, duration:0.15, drawSVG: 0, stagger:.25})
+    .from("#left-arrow",{scale:0, duration:0.15, drawSVG: 0})
+    .from("#up-arrow",{ scale:0, duration:0.15, stagger:0.25, drawSVG: 0})
+    .from("#down-arrow",{ scale:0, duration:0.15, stagger:0.25, drawSVG: 0})
     .from("#star",{scale:4, duration:0.25, drawSVG: 0})
     
 }
@@ -29,11 +41,11 @@ function patternMotion(){
 
 function UIMotion(){
     var tl = gsap.timeline()
-    tl.from(".letter",{duration:0.5, alpha:0, x:20, stagger: 0.25},"-=50%")
-    .from("#box",{drawSVG:"100% 100%"},{duration:1, drawSVG:"50% 50%"},"drawOutline")
-    .from("#fill",{drawSVG:"100% 100%"},{duration:1, drawSVG:"50% 50%"},"drawOutline")
-    .from("#line",{duration:0.25, drawSVG:0})
-    .from("#arrow",{duration:0.25, drawSVG:0})
+    tl.from("#box",{drawSVG:"100% 100%"},{duration:1, drawSVG:"50% 50%"},"drawOutline")
+    .from("#bg",{duration:0.5, scaleX:0})
+    .from(".letter",{duration:0.2, alpha:0, x:20, stagger: 0.2},"-=50%")
+    .from("#arrow",{y: -100, duration:0.25, drawSVG:0})
+    .from("#line",{y:100, duration:0.25, drawSVG:0})
     return tl;
 }
 
